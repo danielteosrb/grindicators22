e128f406ff5d1494830982d4f41e830f7224ae7f
danielteosrb <daniel.nelson-donaghy@executiveoffice-ni.gov.uk>
2023-10-19 09:32:49 +0100

    Changes to fig 5 commentary in line with EH preferences.

--- commentary/fig5.R
+++ commentary/fig5.R
@@ -1,3 +1,4 @@
 f5_projects <- round2(fig5$young[fig5$activity == "Projects"])
 f5_classes <- round2(fig5$young[fig5$activity == "Classes"])
 f5_equipment <- round2(fig5$young[fig5$activity == "Facilities/\nEquipment"])
+f5_all <- colPct(YLT, DONEWHAT5, "Not Ticked")
