3046b6a13c8ecac84ecc63713b5136dd2eb8eba4
danielteosrb <daniel.nelson-donaghy@executiveoffice-ni.gov.uk>
2023-10-19 11:59:32 +0100

    Corrections to fig 13 commentary.

--- commentary/fig13.R
+++ commentary/fig13.R
@@ -22,4 +22,4 @@ f13para1 <- if (f13Sig != FALSE) {
 # This line is hardcoded because we don't know the N for the 2013/14 figure so can't do a significance test. It's unlikely to ever be wrong but it would be worth reviewing
 
 f13para2 <- paste0("Since 2013/14, there has been a significant increase in the percentage of first preference applications to post-primary integrated schools that do not result in admissions to that particular school – an increase of ",
-                   round2(abs(fig13$pct[fig13 == intYear] - fig13$pct[fig13$year == "2013/14"]))," percentage points (", intYear, ": ", round2(fig13$pct[fig13$year == intYear]), "%; 2013: ", round2(fig13$pct[fig13$year == "2013/14"]), "%). This represents an increasing over-subscription to popular post-primary integrated schools.")
+                   round2(abs(fig13$pct[fig13 == intYear] - fig13$pct[fig13$year == "2013/14"]))," percentage points (", intYear, ": ", round2(fig13$pct[fig13$year == intYear]), "%; 2013/14: ", round2(fig13$pct[fig13$year == "2013/14"]), "%). This represents an increasing over-subscription to popular post-primary integrated schools.")
